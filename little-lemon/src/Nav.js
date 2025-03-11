3aa04284401557fe26f242e354ee96e1ec9632ec
Zhao Zhang <zhaosuper123@gmail.com>
2025-03-11 19:24:16 +0000

    init

---
+++ little-lemon/src/Nav.js
@@ -0,0 +1,13 @@
+function Nav() {
+    return (<Nav>
+        <img src="little-lemon/src/image.png"></img>
+        <HStack>
+            <ul>
+                <li><a>Home</a></li>
+                <li><a>About</a></li>
+                <li><a>Menu</a></li>
+                <li><a>Book a Reservation</a></li>
+            </ul>
+        </HStack>
+    </Nav>)
+}
